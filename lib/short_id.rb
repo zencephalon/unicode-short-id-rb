5def2d64e821fb2abb25dee34868bca561510d56
Matthew Bunday <zenceph@gmail.com>
2017-08-21 19:58:32 -0400

    Implementation of short id algorithm

--- lib/short_id.rb
+++ lib/short_id.rb
@@ -1,3 +1,18 @@
-def int_to_short_id(n)
+require_relative 'id_chars.rb'
 
+ID_CHARS_LENGTH = ID_CHARS.length
+
+def int_to_short_id(n)
+  raise "N must be greater than 0" unless n > 0
+  id = ''
+
+  places = Math.log(n, ID_CHARS_LENGTH).floor
+
+  places.downto(0).each do |place|
+    div, remainder = n.divmod(ID_CHARS_LENGTH ** place)
+    id += ID_CHARS[div - 1]
+    n = remainder
+  end
+
+  return id
 end
